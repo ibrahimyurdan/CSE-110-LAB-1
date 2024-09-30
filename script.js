ade53f2e055b356cd613df40847d8082ae60ae3f
ibrahimyurdan <ibyurdan@gmail.com>
2024-09-30 15:05:10 -0700

    Task 5 done

--- script.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {
-    document.getElementById("subscribe-form").addEventListener("submit", alertSubFunc);
-}
-
-function alertSubFunc(event) {
-    event.preventDefault();
-    let emailInput = document.getElementById("email").value;
-    alert("Subscriber added: " + emailInput);
-}
